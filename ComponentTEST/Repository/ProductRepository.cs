160e68b54137bd27c6d16457ed2b6851f7a96b50
annlan08 <annlan08@users.noreply.github.com>
2022-10-07 20:54:11 +0800

    加入專案檔案。

---
+++ ComponentTEST/Repository/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ComponentTEST.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComponentTEST.Repository
+{
+    public class ProductRepository
+    {
+        public async Task<List<Product>> GetProduct()
+        {
+            var q= await (new NorthwindContext()).Products.ToListAsync();
+            return q;
+        }
+
+    }
+}
---
+++ ComponentTEST/ViewComponents/ClassA.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComponentTEST.ViewComponents
+{
+    public class ClassA:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string str ="GO fuck yourself";
+            return View();
+        }
+    }
+}
